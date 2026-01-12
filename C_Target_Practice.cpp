64ca5c5a25d1461ab9fbfeaa08e39e7763d42b1f
Rehan Singh <singh.rehan.2314@gmail.com>
2026-01-12 23:42:50 +0530

    12th jan

--- C_Target_Practice.cpp
+++ C_Target_Practice.cpp
@@ -7,13 +7,14 @@ int main(){
     while(N--){
         int n = 10;
         string arr[n];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){//input 10x10 grid
                 cin>>arr[i];
         }
         int score = 0;
         for(int i=0;i<n;i++){
             for (int j=0;j<n;j++){
                 if(arr[i][j]=='X'){
+                    // Calculate distance to nearest edge
                     int rowDist = min(i, n - 1 - i);
                     int colDist = min(j, n - 1 - j);
                     int points = min(rowDist, colDist) + 1;
